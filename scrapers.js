02c7c21fb27784978c3d2448fc126ba7bef00575
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-17 17:54:18 -0500

    add logic to scrape specified webpage/elements

--- scrapers.js
+++ scrapers.js
@@ -6,6 +6,34 @@ const scrapeProduct = async (url) => {
   const browser = await puppeteer.launch(); // start browser
   const page = await browser.newPage(); // start page
   await page.goto(url); // get page of input url
+
+  // Get element of webpage you wish to scrape in chrome console
+  // Right click the element > Copy > XPath
+
+  const [element] = await page.$x(
+    "/html/body/div[1]/div[2]/div[4]/div[1]/div[6]/div[1]/div[1]/div[2]/span/div/img"
+  ); // $x() -> puppeteer selector returns array
+  const srcText = await element.getProperty("src");
+  const src = await srcText.jsonValue();
+
+  // To scrape anything takes 3 commands:
+  // 1. select the xpath of the element you wish to scrape
+  // 2. call getProperty() on the result of the elemtn to get the "textContent"
+  // 3. extract the json value
+
+  const [element2] = await page.$x("//*[@id='productTitle']"); // $x() -> puppeteer selector returns array
+  const titleText = await element2.getProperty("textContent");
+  const title = await titleText.jsonValue();
+
+  const [element3] = await page.$x(
+    "/html/body/div[1]/div[2]/div[4]/div[1]/div[7]/div[16]/div[2]/div[2]/ul/li[2]/span/span[1]/span/a/span[2]/span"
+  ); // $x() -> puppeteer selector returns array
+  const priceText = await element3.getProperty("textContent");
+  const price = await priceText.jsonValue();
+
+  console.log({ src, title, price });
+
+  browser.close(); // close puppeteer browser connection
 };
 
 const url =
